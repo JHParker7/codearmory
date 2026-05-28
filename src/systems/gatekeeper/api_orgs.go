5fb0fbbe3ab62a74f24daa4ea00427b759717a65
jhparker7 <github@jhparker.uk>
2026-05-28 18:09:28 +0100

    fix(infra): forge docker GID, registry manifest mount, org blueprints resource path

--- src/systems/gatekeeper/api_orgs.go
+++ src/systems/gatekeeper/api_orgs.go
@@ -149,7 +149,7 @@ func handleCreateOrg(w http.ResponseWriter, r *http.Request) {
 	bpPermName := fmt.Sprintf("%s-%s-blueprints-state", owner.Username, org.OrgName)
 	if err := grantServicePermissions(ctx, connect().WithContext(ctx), "blueprints", userID, bpPermName,
 		[]string{"getState", "updateState", "deleteState", "lockState", "unlockState"},
-		fmt.Sprintf("blueprints/orgs/%s/states/*", org.OrgID)); err != nil {
+		fmt.Sprintf("blueprints/%s/states/*", org.OrgName)); err != nil {
 		slog.Warn("create org: failed to grant blueprints state permission", "caller_id", callerID, "org_id", org.OrgID, "error", err)
 	} else {
 		slog.Info("create org: blueprints state permission granted", "caller_id", callerID, "org_id", org.OrgID)
